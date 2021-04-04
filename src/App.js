44f3b2cd2c1f697639b7fe9afe6fbe941edac9b6
marina saber <marina.bergas@gmail.com>
2021-04-04 02:24:06 +0200

    Add Task

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import './App.css';
 import { Header } from './component/Header';
 import {useState} from 'react';
 import Tasks from './component/Tasks';
+import AddTask from './component/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id:'1',
-      text:'marina'
+      text:'marina',
+      reminder:true
     },
     {
       id:'2',
-      text:'sameria'
+      text:'sameria',
+      reminder:true
     },
     {
       id:'3',
-      text:'demiana'
+      text:'demiana',
+      reminder:false
     }
   ])
+  const DeleteTask=(id)=>{
+    setTasks(tasks.filter(task=>task.id!==id))
+ }
+ const toggleReminder = (id)=>{
+    setTasks(tasks.map(task=>task.id === id ?{...task,reminder:!task.reminder}:task))
+ }
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <Tasks tasks={tasks}/>
+        <AddTask/>
+        {tasks.length>0?<Tasks tasks={tasks} onDelete={DeleteTask} onToggle={toggleReminder}/>:'No Tasks To Show'}
       </div>
     </div>
   );
